91853fe7b9f16c7a1d9867cbb0bb140dc01ff453
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-10-11 17:43:25 +0530

    commit

---
+++ activity5.py
@@ -0,0 +1,10 @@
+x=("birth date":12-08-2015)
+y="birth month:august"
+z="mothers name: Sweta gupta"
+a="fathers name: Umesh kumar gupta"
+b="child name: Rishit gupta"
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
