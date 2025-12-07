9cc055a220cac355b35d2186b6fb4485f0bf4323
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-12-07 15:38:55 +0530

    tero

---
+++ lesson2/activity15.py
@@ -0,0 +1,5 @@
+import turtle
+turtle.Screen(). bgcolor("blue")
+circle=turtle.Turtle()
+circle.shape('circle')
+turtle.done()
---
+++ lesson2/activity16.py
@@ -0,0 +1,4 @@
+def coc(r):
+    return 2*3.14*r
+radius=int(input("please enter the radius of the circle: "))
+print(coc(radius))
