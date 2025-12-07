2d7e9ec020df3da3061498f801ca62ff3e1ad170
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-12-07 14:37:53 +0530

    ay mi gatito

---
+++ lesson2/activity14.py
@@ -0,0 +1,4 @@
+def aoc(r):
+    return r*r*3.14
+radii=int(input(" please enter the radius of the circle: "))
+print(aoc(radii))
--- lesson2/activity6.py
+++ lesson2/activity6.py
@@ -1,11 +1,30 @@
-rowNum = 3 #initialised number of rows 5
-
-space = rowNum-1
-
-for i in range(1, rowNum+1): #loops for rows
-    for j in range(1,space+1):
-        print(end="")
+#take input from user
+rowsize=int(input(" enter number of rows: "))
+if rowsize%2==0: #conditons
+    halfdiamonrow=int(rowsize/2)
+else:
+    halfdiamonrow=int(rowsize/2)+1
+space=halfdiamonrow-1
+#loop for upper part
+for i in range(1,halfdiamonrow+1): #loop for rows
+    for j in range(1,space+1): #loop for coloumns
+        print(end=" ")
     space-=1
+    num=1
     for j in range(2*i-1):
-        print(end="*")
+        print(end=str(num))
+    #incrementing number in each coloumn
+        num+=1
+    print()
+space=1
+#loop for lower part
+for i in range(1,halfdiamonrow): #loop for rows
+    for j in range(1,space+1): #loop for coloumns
+        print(end=" ")
+    space+=1
+    num=1
+    for j in range(1,2*(halfdiamonrow-i)):
+        print(end=str(num)) #display result
+    #incrementing number in each coloumn
+        num+=1
     print()
