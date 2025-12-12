972c66f1cc5fd0f3de8d8f3011f86ebd647d6d49
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-12-12 16:48:33 +0530

    kgjgj

--- lesson2/activity16.py
+++ lesson2/activity16.py
@@ -1,4 +1,4 @@
-def coc(r):
-    return 2*3.14*r
-radius=int(input("please enter the radius of the circle: "))
-print(coc(radius))
+def coc(r): #function   
+    return 2*3.14*r #the formula
+radius=int(input("please enter the radius of the circle: ")) #taking the input from the user
+print("the circumference of a circle with radius",radius,"is",coc(radius)) #display result
