ae8c5d081c139db922447e98c11c018d60390e86
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-12-21 15:39:39 +0530

    nwsg

---
+++ lesson2/activity21.py
@@ -0,0 +1,8 @@
+#take input
+a=input(" please enter a word(in capital letters only): ")
+for i in a:#loop iteration
+    if (i=="A"):#condition
+        print("A is found")
+        break
+    else:
+        print("A not found")#display result
---
+++ lesson2/activty24.py
@@ -0,0 +1,3 @@
+def func():
+    for i in range(11):
+        if i%2==
