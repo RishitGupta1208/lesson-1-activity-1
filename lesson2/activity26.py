254baa2584dc2545d2457ba07b957460fc4871c9
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-12-24 16:29:29 +0530

    ewqieoi

---
+++ lesson2/activity26.py
@@ -0,0 +1,4 @@
+Aohi=2.50
+Avhtp=4.00
+Aswr=Avhtp-Aohi
+print("the shopkeeper has to return",Aswr,"$ to Vishnu")
---
+++ lesson2/activity27.py
@@ -0,0 +1,5 @@
+try:
+    number=int(input("please enter a number:"))
+    print("4")
+except ValueError as axis:
+    print("exception:",axis)
