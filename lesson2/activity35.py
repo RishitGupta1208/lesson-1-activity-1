fcbdc383aa5b5024b8850dea1cd42f7b91b9ce6c
rishitgupta1208 <rishitgupta1208@gmail.com>
2026-01-05 17:12:04 +0530

    xcd

---
+++ lesson2/activity35.py
@@ -0,0 +1,6 @@
+copt=int(input("what is the cost of the plane tickets: "))
+pptthpd=int(input("what is the price you paid to the hotel per day: "))
+hmdsih=int(input(" how many days stayed in hotel: "))
+hmpfv=int(input(" how much paid for the vehicle:"))
+te=pptthpd*hmdsih+copt+hmpfv
+print("the total trip expenditure is",te)
