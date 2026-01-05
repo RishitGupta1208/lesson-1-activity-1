44333c7da1e9bd7fe5f2ccc8a937cb9b5ec44733
rishitgupta1208 <rishitgupta1208@gmail.com>
2026-01-05 15:58:31 +0530

    i

---
+++ lesson2/activity32.py
@@ -0,0 +1,9 @@
+hy=int(input(" please enter the value of hypotenus(in cm): "))
+al=int(input("please enter the value of altitude(in cm): "))
+ba=int(input(" please enter the value of base(in cm): "))
+sin=hy/al
+cos=hy/ba
+tan=sin/cos
+print("sin of this triangle is",sin)
+print("cos of this triangle is",cos)
+print("tan of this triangle is",tan)
