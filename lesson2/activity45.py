991adb93e2991f2c32c0ac44ca8970b64498c528
rishitgupta1208 <rishitgupta1208@gmail.com>
2026-01-15 09:30:51 +0530

    8ids

---
+++ lesson2/activity45.py
@@ -0,0 +1,8 @@
+test_dict={'codingal':2,'is':2,'best':2,'for':2,'coding':1}
+print("the orignal dictionary:"+str(test_dict))
+k=2
+res=0
+for key in test_dict:
+    if test_dict[key]==k:
+        res+=1
+print("Frequency of k is:" +str(res))
